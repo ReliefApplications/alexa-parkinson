d1602e94f26faea720d6b4ed4afcd10d8f3d920b
Odysseas Diamadis <odysseas.reliefapps@gmail.com>
2019-06-19 09:13:21 +0200

    added some comments on dialogue-tree

--- src/lib/dialogue/dialogue-tree.js
+++ src/lib/dialogue/dialogue-tree.js
@@ -10,42 +10,46 @@ module.exports.trees = (function () {
      */
     function State(callback, yes, no, didNotUnderstand) {
 
+        /** */
         this.yes = yes instanceof Function ? yes : () => {};
         this.no = no instanceof Function ? no : () => {};
         this.didNotUnderstand = didNotUnderstand instanceof Function ? didNotUnderstand : () => {};
         this.action = callback instanceof Function ? callback : () => {};
 
         this.children = {};
+    }
+    
 
-        // Useful?
-        State.prototype.setAction = function (callback) {
-            this.action = callback;
-        }
+    /**
+     * @param {Function} callback - function to execute when entering the state
+     */
+    State.prototype.setAction = function (callback) {
+        this.action = callback;
+    }
 
-        State.prototype.addChild = function (hash, node) {
-            if (node instanceof StateTree) {
-                this.children[hash] = node.rootNode
-            } else {
-                this.children[hash] = node;
-            }
-            return this;
+    State.prototype.addChild = function (hash, node) {
+        if (node instanceof StateTree) {
+            this.children[hash] = node.rootNode
+        } else {
+            this.children[hash] = node;
         }
+        return this;
+    }
 
-        State.prototype.do = function (request, response, ...params) {
-            this.action(request, response);
-        }
+    State.prototype.do = function (request, response, ...params) {
+        this.action(request, response);
+    }
 
-        State.prototype.saidYes = function(request, response) {
-            this.yes(request, response);
-        }
+    State.prototype.saidYes = function(request, response) {
+        this.yes(request, response);
+    }
 
-        State.prototype.saidNo = function(request, response) {
-            this.no(request, response);
-        }
+    State.prototype.saidNo = function(request, response) {
+        this.no(request, response);
+    }
 
-        State.prototype.didNotUnderstand = function(request, response) {
-            this.didNotUnderstand(request, response);
-        }
+    State.prototype.didNotUnderstand = function(request, response) {
+        this.didNotUnderstand(request, response);
     }
 
     /**
@@ -56,47 +60,51 @@ module.exports.trees = (function () {
         this.rootNode = new State(rootCallback);
         this.currentNode = this.rootNode;
         // this.nodes = {}
-
-        StateTree.prototype.addIntentAction = function (stateName, node) {
-            this.rootNode.addChild(stateName, node);
-            return this;
-        }
-
-        StateTree.prototype.navigateTo = function (stateName, request, response) {
-            // Take what is supposed to be the next node
-            let nextNode = this.currentNode.children[stateName.toString()];
-            // If it's not a child
-            if (nextNode === undefined) {
-                // Should be a in-order search
-                this.currentNode = this.rootNode;
-                nextNode = this.currentNode.children[stateName.toString()];
-                // console.log("After check", nextNode);
-            } else if (nextNode instanceof StateTree) {
-                nextNode = nextNode.rootNode;
-                nextNode = this.currentNode.children[stateName.toString()];
-            }
-
-            this.currentNode = nextNode;
-            // If nextNode is still undefined, it's not a child of the root node
-            return this.currentNode.do(request, response);
-        }
-
-        StateTree.prototype.setRootAction = function (callback) {
-            this.rootNode.setAction(callback);
-        }
-
-        StateTree.prototype.start = function () {
-            debugger;
-            this.rootNode.do();
-        }
-
-        StateTree.prototype.saidYes = function(request, response) { this.currentNode.saidYes(request, response); }
-        StateTree.prototype.saidNo = function(request, response) { this.currentNode.saidNo(request, response); }
-        StateTree.prototype.didNotUnderstand = function(request, response) { this.currentNode.didNotUnderstand(request, response); }
     }
+
+    /**
+     * @param {string} stateName - Name of the child intent
+     * @param {State} state - State associated with the stateName
+     */
+    StateTree.prototype.addIntentAction = function (stateName, state) {
+        this.rootNode.addChild(stateName, state);
+        return this;
+    }
+
+    StateTree.prototype.navigateTo = function (stateName, request, response) {
+        // Take what is supposed to be the next node
+        let nextNode = this.currentNode.children[stateName.toString()];
+        // If it's not a child
+        if (nextNode === undefined) {
+            // Should be a in-order search
+            this.currentNode = this.rootNode;
+            nextNode = this.currentNode.children[stateName.toString()];
+            // console.log("After check", nextNode);
+        } else if (nextNode instanceof StateTree) {
+            nextNode = nextNode.rootNode;
+            nextNode = this.currentNode.children[stateName.toString()];
+        }
+        this.currentNode = nextNode;
+        // If nextNode is still undefined, it's not a child of the root node
+        return this.currentNode.do(request, response);
+    }
+
+    StateTree.prototype.setRootAction = function (callback) {
+        this.rootNode.setAction(callback);
+    }
+
+    StateTree.prototype.start = function () {
+        this.rootNode.do();
+    }
+
+    StateTree.prototype.saidYes = function(request, response) { this.currentNode.saidYes(request, response); }
+    StateTree.prototype.saidNo = function(request, response) { this.currentNode.saidNo(request, response); }
+    StateTree.prototype.didNotUnderstand = function(request, response) { this.currentNode.didNotUnderstand(request, response); }
+    
 
     return {
         State: State,
         StateTree: StateTree
     };
+
 })()
