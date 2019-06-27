37b26a6c7e5a3ae7e556112699b1241b48974fcc
Odysseas Diamadis <odysseas.reliefapps@gmail.com>
2019-06-27 11:14:35 +0200

    Included webpack to minimize the build

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+const {
+  NODE_ENV = 'production',
+} = process.env;
+
+module.exports = {
+  entry: './app.js',
+  mode: NODE_ENV,
+  externals: [ nodeExternals() ],
+  target: 'node',
+  resolve: {
+    extensions: ['.js']
+  },
+  output: {
+    filename: 'alexa-parkinson.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
