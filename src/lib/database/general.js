760330d6a5c9d787842cff96311e9ad98195b582
Odysseas Diamadis <odysseas.reliefapps@gmail.com>
2019-07-10 14:41:52 +0200

    Added check of credentials' presence in the configuration

--- src/lib/database/general.js
+++ src/lib/database/general.js
@@ -11,8 +11,15 @@ module.exports = {
     */
     openDatabase: function () {
         return new Promise(function (resolve, reject) {
+
+            // If there are both username and password into the configurations, load them both to authenticate the password
+            // Otherwise, do not use any credential. Used for testing;
+            let optionObject = (configuration.database.username === undefined || configuration.database.password === undefined) ? 
+                { useNewUrlParser: true } :
+                { useNewUrlParser: true, auth: { user: configuration.database.username, password: configuration.database.password }};
+
             mongo.connect(configuration.database.url,
-                { useNewUrlParser: true, auth: { user:configuration.database.username , password:configuration.database.password } },
+                optionObject,
                 function (error, client) {
                     if (error !== null) {
                         reject(error);
