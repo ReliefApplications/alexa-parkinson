5a0efc8604a0c94afb93ed11c40e025c3bdddbc1
Odysseas Diamadis <odysseas.reliefapps@gmail.com>
2019-07-22 09:31:01 +0200

    Added explanation of the example in alexa-dialogue.js

--- src/lib/dialogue/alexa-dialogue.js
+++ src/lib/dialogue/alexa-dialogue.js
@@ -1,3 +1,19 @@
+/**
+ * This is an example of a dialogue node.
+ * 
+ * Below are grouped all the nodes in the scenarios/ folder together with their 'hash' name.
+ * 
+ * In myMedication three functions are defined: main (mandatory), yes, now and "did not understand"
+ * 
+ * Later the myMedication node is added to the dialogue tree with the 'myMedication' name
+ * 
+ * In this example, the main function takes both the request and the response.
+ * This was just an experiment and an
+ * example, we should check slot values in the intent handler and pass only
+ * relevant informations to respect a sort of MVC pattern.
+ */
+
+
 const tree = require('./dialogue-tree').trees;
 
 const Utils = require('../../Utils').Utils;
@@ -15,11 +31,6 @@ const texts = Constants.TEXTS;
 const images = Constants.IMAGES;
 
 
-
-const dialogue = new tree.StateTree({
-    // Root callback (if not defined throws an error)
-    main: () => {}
-});
 const State = tree.State;
 
 const myMedication = new State({
@@ -36,20 +47,29 @@ const myMedication = new State({
                 );
             }
             response.shouldEndSession(false);
-
+            
         },
         
-    yes: (request, response) => {
-        response.shouldEndSession(false);
-    },
-    
-    no: (request, response) => {
-        response.shouldEndSession(false);
-    },
-    
-    didNotUnderstand: (request, response) => {
-        response.shouldEndSession(false);
-    }
+        yes: (request, response) => {
+            response.shouldEndSession(false);
+        },
+        
+        no: (request, response) => {
+            response.shouldEndSession(false);
+        },
+        
+        didNotUnderstand: (request, response) => {
+            response.shouldEndSession(false);
+        }
+});
+
+
+/**
+ * Tree dialogue that handles all the conversation
+ */
+const dialogue = new tree.StateTree({
+    // Root callback (if not defined throws an error)
+    main: () => {}
 });
 
 dialogue.addIntentAction('myMedication', myMedication);
