97779a338518d1c8df94a57855d0141cc09fda76
Odysseas Diamadis <odysseas.reliefapps@gmail.com>
2019-07-24 12:00:15 +0200

    Updated app to get environment from configuration

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var alexa = require('alexa-app');
 var PORT = process.env.PORT || 12118;
 var app = express();
 
+const env = require('./src/configurations').environment;
+const isProduction = env.production === 'true';
+
 // You choose here what will be the endpoint for Alexa. If empty it will be the root: '/'
 var alexaApp = new alexa.app('/');
 alexaApp.express({
@@ -13,11 +16,11 @@ alexaApp.express({
     // verifies requests come from amazon alexa. Must be enabled for production.
     // You can disable this if you're running a dev environment and want to POST
     // things to test behavior. enabled by default.
-    checkCert: false,
+    checkCert: isProduction,
 
     // sets up a GET route when set to true. This is handy for testing in
     // development, but not recommended for production. disabled by default
-    debug: false
+    debug: (!isProduction)
 });
 
 var alexa = require('./index.js').handler(alexaApp);
