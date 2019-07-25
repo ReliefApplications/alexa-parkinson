f1e91c9e3ad44e840ab59ddf004124d9d310816a
Odysseas Diamadis <odysseas.reliefapps@gmail.com>
2019-07-25 13:52:17 +0200

    Removed old variables

--- src/Constants.js
+++ src/Constants.js
@@ -5,55 +5,43 @@ module.exports = {
       too_many_medicines: "Tengo mas de un medicamento con este nombre.",
       medicine_already_here: "Medicamento añadido a tu calendario ¿Quieres añadir otro?"
     },
-
+    
+    //--- WELCOME ---
     welcome: {
       title: ["Bienvenido a la skill de Asistencia Parkinson."],
       text: ["Queremos ofrecerte toda la información sobre tu medicación además de darte la posibilidad de consultar tus dudas con la Asociación Parkinson Madrid. \nDi “Mi Medicación” o “Llamar”"],
       reprompt: ["Di “Mi Medicación”, “Llamar” o pregúntame “¿Qué puedo hacer?”"],
     },
-    //--- WELCOME ---
-    welcomeTitle: "Bienvenido a la skill de Asistencia Parkinson.",
-    welcomeText: "Queremos ofrecerte toda la información sobre tu medicación además de darte la posibilidad de consultar tus dudas con la Asociación Parkinson Madrid. \nDi “Mi Medicación” o “Llamar”",
-    welcomeReprompt: "Di “Mi Medicación”, “Llamar” o pregúntame “¿Qué puedo hacer?”",
 
+    //--- MyMedication ---
     mymedication: {
       title: ["Mi medicación"],
       text: ["Ok, pregúntame por tu medicación programada. Por ejemplo di: ¿Qué medicación tengo que tomar hoy? O pregúntame “¿Qué puedo hacer?”"],
       reprompt: ["Disculpa ¿Quieres información sobre tu medicación de hoy?"]
     },
-    //--- MyMedication ---
-    myMedicationTitle: "Mi medicación",
-    myMedicationText: "Ok, pregúntame por tu medicación programada. Por ejemplo di: ¿Qué medicación tengo que tomar hoy? O pregúntame “¿Qué puedo hacer?”",
-    myMedicationReprompt: "Disculpa ¿Quieres información sobre tu medicación de hoy?",
 
+    //--- Call ---
     call: {
       title: ["Llamada"],
       text: ["Informacion sobre la llamada. Quieres llamar?"],
       reprompt: ["llamada repete"],
     },
-    //--- Call ---
-    callTitle: "Llamada",
-    callText: "Informacion sobre la llamada. Quieres llamar?", // Do you want to call the association?
-    callReprompt: "llamada repete", // Do you want to call the association?
 
+    //--- CompleteMedicineInserton ---
     medicineinsertion: {
       title: [],
       text: ["Medicamento añadido a tu calendario ¿Quieres añadir otro?"],
       reprompt: [],
     },
-    //--- CompleteMedicineInserton ---
     insertionText: "Medicamento añadido a tu calendario ¿Quieres añadir otro?",
 
+    //--- Help ---
     help: {
       speech: [],
       title: ["Que puede hacer"],
       text: ["Puedes crear un calendario de medicación. Di por ejemplo “Quiero añadir sinemet”. Puedes preguntar qué medicación tienes en tu calendario. Di por ejemplo “¿Qué medicamentos tengo que tomar hoy?”. También puedes obtener información sobre cualquier medicación relacionada con el Parkinson. Di por ejemplo: “Efectos secundarios del Sinemed”. Además puedes llamar a la asociación Parkinson Madrid, Di “Llamar a la Asociación”."],
       reprompt: [],
     },
-    //--- Help ---
-    helpText: "Puedes crear un calendario de medicación. Di por ejemplo “Quiero añadir sinemet”. Puedes preguntar qué medicación tienes en tu calendario. Di por ejemplo “¿Qué medicamentos tengo que tomar hoy?”. También puedes obtener información sobre cualquier medicación relacionada con el Parkinson. Di por ejemplo: “Efectos secundarios del Sinemed”. Además puedes llamar a la asociación Parkinson Madrid, Di “Llamar a la Asociación”.",
-    helpScreenTitle: "Que puede hacer",
-    helpScreenText: "\"Quiero añadir sinemet cada dia\" o \"¿Que tengo que tomar hoy?\"",
 
     //--- Unhandled DEFAULT ---
     unhandled: {
@@ -61,9 +49,6 @@ module.exports = {
       text: ["Lo siento, no te he entendido."],
       reprompt: ["Lo siento, no te he entendido."],
     },
-    unhandledDefaultTitle:"Solicitud desconocida.",
-    unhandledDefaultText:"Lo siento, no te he entendido.",
-    unhandledDefaultReprompt:"Lo siento, no te he entendido.",
 
     //--- Unhandled Launch ---
     unhandledLaunch: {
@@ -71,8 +56,6 @@ module.exports = {
       text: [],
       reprompt: [],
     },
-    unhandledLaunchText1:"Quieres información sobre tu medicación?",
-    unhandledLaunchText2:"Quieres realizar una llamada al servicio de asistencia de Parkinson?",
 
     //--- Unhandled MyMedication ---
     unhandledMyMedicationText:"Disculpa ¿Quieres información sobre tu medicación de hoy?",
